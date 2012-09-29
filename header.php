ed0d2781ffd83d7a4902a1684d7b23822bb9ab67
Alex Chiang <alex@chizang.net>
2012-09-28 18:28:22 -0700

    header.php: add support for favicon
    Signed-off-by: Alex Chiang <alex@chizang.net>

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
+<link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" />
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <title><?php if ( is_single() ) {
